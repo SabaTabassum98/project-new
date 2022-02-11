80c1aa426354e971b16d2d6ffc03d3669583f059
SabaTabassum98 <khalidzeeba@gmail.com>
2022-02-11 23:40:55 +0530

    first commit

---
+++ src/pack/vowelOrConsonant.java
@@ -0,0 +1,26 @@
+package pack;
+import java.util.Scanner;
+
+public class vowelOrConsonant {
+	
+		void findVowelOrNot(char ch)
+		{
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+			{
+			System.out.println("Entered character "+ch+" is  Vowel"); 
+			}
+			else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+				System.out.println("Entered character "+ch+" is Consonent");
+			      else
+				System.out.println("Not an alphabet");		
+		}
+		public static void main(String[ ] arg)
+		{
+			vowelOrConsonant c=new vowelOrConsonant();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter a character : ");
+			char in=sc.next( ).charAt(0);	 		
+			c.findVowelOrNot(in);
+		}
+	}
+
